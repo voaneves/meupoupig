e28516537beeb1b130c45706e01ef4c20b891284
Victor Neves <victorneves478@gmail.com>
2024-04-01 21:00:06 -0300

    Adding the quiz page and reverting changes in last commit

--- quiz/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker: Installed");
-});
-
-self.addEventListener("fetch", (e) => {
-  console.log("Service Worker: Fetching");
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
